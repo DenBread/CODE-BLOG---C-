ec36b7d80025a44531212fcbc734ed819d5c121f
Den <den26544@mail.ru>
2020-04-17 04:07:24 +0300

    Den.CodeBlog
    Обновил именна. Сделал отдельный класс с эффектом печатной машинкой. Его можно вызвать спокойно через конструктор.
    Так же протектил конструкторы.

---
+++ Den.CodeBlog/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Den.CodeBlog
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TypeWriter typeWriter = new TypeWriter(new []{
+                 "Проснись, Ильюх...",
+                 "Матрица нуждаеться в тебе...",
+                 "Следуй за кроликом.",
+                 "Тук, тук, Илья."
+                });
+
+            
+
+            Console.ReadLine();
+        }
+    
+    }
+}
---
+++ Den.CodeBlog/TestContructer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Den.CodeBlog
+{
+    /// <summary>
+    /// Тестовый класс для пробы конструкторов
+    /// </summary>
+    class TestContructer
+    {
+        private string _name;
+        private int _age;
+
+        public TestContructer() { }
+
+        /// <summary>
+        /// Это тестовый конструктор
+        /// </summary>
+        /// <param name="name">Пишем имя человека</param>
+        public TestContructer(string name)
+        {
+            _name = name;
+        }
+
+
+        /// <summary>
+        /// Второй конструктор
+        /// </summary>
+        /// <param name="n">Имя человека</param>
+        /// <param name="a">Возраст человека</param>
+        public TestContructer(string n, int a) : this(n)
+        {
+            _age = a;
+        }
+
+        /// <summary>
+        /// Информация о человеке
+        /// </summary>
+        public void Info()
+        {
+            Console.WriteLine($"{_name} - {_age}");
+
+            
+        }
+    }
+}
